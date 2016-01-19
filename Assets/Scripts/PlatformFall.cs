8dc5ac7882d13675c829c8b867b3a9de5d4f790d
Jon Adler <jadler2660@gmail.com>
2016-01-18 20:29:51 -0800

    first commit. built tutorial game.

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFall : MonoBehaviour {
+
+    public float fallDelay = 1;
+
+    private Rigidbody2D rb2d;
+
+	
+	void Awake () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/SimplePlatformController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimplePlatformController : MonoBehaviour {
+
+    [HideInInspector]
+    public bool facingRight = true;
+
+    [HideInInspector]
+    public bool willJump = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundCheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            willJump = true;
+        }
+	}
+
+    void FixedUpdate ()
+    {
+        float horizonalAxis = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(horizonalAxis));
+
+        if(horizonalAxis * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * horizonalAxis * moveForce);
+        }
+
+        if(Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+
+        if(horizonalAxis > 0 && !facingRight)
+        {
+            Flip();
+        } else if (horizonalAxis < 0 && facingRight) {
+            Flip();
+        }
+
+        if(willJump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            willJump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 tempScale = transform.localScale;
+        tempScale.x *= -1;
+        transform.localScale = tempScale;
+    }
+}
